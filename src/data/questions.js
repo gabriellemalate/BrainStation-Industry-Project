8e17cd14b95b2443bc1060680b91154423348516
Stephen Man <stephenman88@gmail.com>
2024-01-26 11:05:20 -0500

    styling fixes for questions and loading page

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,18 +1,18 @@
 const questionData = [
-    {id: "climate", 1: "warm", 2: "cold", question: "Warm or cold?"},
-    {id: "outdoors", 1: "culture", 2: "wilderness", question: "Culture or wilderness?"},
-    {id: "household", 1: "family", 2: "partner", question: "Family or Partner?"},
-    {id: "party", 1: "party", 2: "laidback", question: "Party or laid back? "},
-    {id: "income", 1: "luxury", 2: "budget", question: "Luxury or Budget?"},
-    {id: "length", 1: "short", 2: "long", question: "Short or long?"},
-    {id: "adventure", 1: "yes", 2: "no", question: "Are you adventurous?"},
-    {id: "spa", 1: "yes", 2: "no", question: "Do you want to treat yourself at the Spa?"},
-    {id: "environmental", 1: "yes", 2: "no", question: "Do you care about environmental sustainability?"},
-    {id: "gaming", 1: "yes", 2: "no", question: "Do you like gaming?"},
-    {id: "food", 1: "yes", 2: "no", question: "Do you like good food?"},
-    {id: "fitness", 1: "yes", 2: "no", question: "Do you care about fitness?  "},
-    {id: "shop", 1: "yes", 2: "no", question: "Do you want to shop?"},
-    {id: "lodging", 1: "yes", 2: "no", question: "Will you need a place to stay before or after the cruise? "}
+    {id: "climate", 1: "Warm", 2: "Cold", question: "Warm or cold?"},
+    {id: "outdoors", 1: "Culture", 2: "Wilderness", question: "Culture or wilderness?"},
+    {id: "household", 1: "Family", 2: "Partner", question: "Family or Partner?"},
+    {id: "party", 1: "Party", 2: "Laidback", question: "Party or laid back? "},
+    {id: "income", 1: "Luxury", 2: "Budget", question: "Luxury or Budget?"},
+    {id: "length", 1: "Short", 2: "Long", question: "Short or long?"},
+    {id: "adventure", 1: "Yes", 2: "No", question: "Are you adventurous?"},
+    {id: "spa", 1: "Yes", 2: "no", question: "Do you want to treat yourself at the Spa?"},
+    {id: "environmental", 1: "Yes", 2: "No", question: "Do you care about environmental sustainability?"},
+    {id: "gaming", 1: "Yes", 2: "No", question: "Do you like gaming?"},
+    {id: "food", 1: "Yes", 2: "No", question: "Do you like good food?"},
+    {id: "fitness", 1: "Yes", 2: "No", question: "Do you care about fitness?  "},
+    {id: "shop", 1: "Yes", 2: "No", question: "Do you want to shop?"},
+    {id: "lodging", 1: "Yes", 2: "No", question: "Will you need a place to stay before or after the cruise? "}
 ]
 
 export default questionData;
