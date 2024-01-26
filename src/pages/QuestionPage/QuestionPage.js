298e47ff18fcc1f26999a0dc2e7a189f46051fa3
Stephen Man <stephenman88@gmail.com>
2024-01-26 02:50:36 -0500

    fixed buttons

--- src/pages/QuestionPage/QuestionPage.js
+++ src/pages/QuestionPage/QuestionPage.js
@@ -26,9 +26,8 @@ function QuestionPage({setCruises, cruises}){
                 localStorage.setItem(questionData.id, choice)
             }
             if(questionId < questionData.length - 1){
-                navigate(`/${questionId + 1}`)
-            }
-            if(questionId < questionData.length){
+                navigate(`/${Number(questionId) + 1}`)
+            }else if(questionId < questionData.length){
                 let requestObject = {};
     
                 questionData.forEach(question => {
@@ -50,7 +49,8 @@ function QuestionPage({setCruises, cruises}){
     }
 
     const onBackPage = () =>{
-        navigate(`/${questionId - 1}`)
+        if(Number(questionId) > 0){navigate(`/${questionId - 1}`)}
+        else{navigate('/')}
     }
 
     const progressBar = () => {
@@ -62,7 +62,6 @@ function QuestionPage({setCruises, cruises}){
         }
         return ''
     }
-
     return (
         <main className='main'>
             <div className='question-page'>
@@ -72,8 +71,12 @@ function QuestionPage({setCruises, cruises}){
                         {progressBar()}
                     </div>
                     <p className='question-page-window__question'>{questionId ? questionData[questionId].question : `Hi, I'm the ocean oracle. Before we get into it, I'd like to ask you questions`}</p>
-                    <button className='question-page-window__button' onClick={(e) => {toNextPage(questionData[questionId]["1"])}}>{questionId ? questionData[questionId]["1"] :"Yes"}</button>
-                    <button className='question-page-window__button' onClick={(e) => {toNextPage(questionData[questionId]["1"])}}>{questionId ? questionData[questionId]["2"] : "No"}</button>
+                    <button className='question-page-window__button' onClick={(e) => {
+                        if(questionId) {toNextPage(questionData[questionId][1])} else {toNextPage(0)}
+                        }}>{questionId ? questionData[questionId][1] :"Yes"}</button>
+                    <button className='question-page-window__button' onClick={(e) => {
+                        if(questionId) {toNextPage(questionData[questionId][2])} else {toNextPage(0)}
+                        }}>{questionId ? questionData[questionId][2] : "No"}</button>
                     {questionId ? <button className='question-page-window__back' onClick={() => {onBackPage()}}>Back</button> : ''}
                 </section>
             </div>
